1a761d1b7689929ddb18b902a541e4e366bce925
JennyShalai <jenny.shalai@gmail.com>
2016-09-08 17:00:01 -0400

    add imgs, add imgs movement, collision detection

--- educational-game-for-kids/ViewController.swift
+++ educational-game-for-kids/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var greenBox: UIImageView!
-    @IBOutlet weak var blueBox: UIImageView!
     
+    @IBOutlet weak var bear: UIImageView!
+    @IBOutlet weak var chicken: UIImageView!
+    @IBOutlet weak var crocodile: UIImageView!
+    @IBOutlet weak var cuala: UIImageView!
+    @IBOutlet weak var monkey: UIImageView!
+    @IBOutlet weak var raccoon: UIImageView!
+    @IBOutlet weak var chickenShadow: UIImageView!
+    @IBOutlet weak var raccoonShadow: UIImageView!
+    @IBOutlet weak var monkeyShadow: UIImageView!
+    @IBOutlet weak var cualaShadow: UIImageView!
+    @IBOutlet weak var crocodileShadow: UIImageView!
+    @IBOutlet weak var bearShadow: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,8 +32,23 @@ class ViewController: UIViewController {
         for touch in touches {
             let location = touch.locationInView(self.view)
             
-            if self.greenBox.frame.contains(location) {
-                self.greenBox.center = location
+            if self.bear.frame.contains(location) {
+                self.bear.center = location
+            }
+            if self.chicken.frame.contains(location) {
+                self.chicken.center = location
+            }
+            if self.crocodile.frame.contains(location) {
+                self.crocodile.center = location
+            }
+            if self.cuala.frame.contains(location) {
+                self.cuala.center = location
+            }
+            if self.monkey.frame.contains(location) {
+                self.monkey.center = location
+            }
+            if self.raccoon.frame.contains(location) {
+                self.raccoon.center = location
             }
         }
     }
@@ -33,8 +58,42 @@ class ViewController: UIViewController {
         for touch in touches {
             let location = touch.locationInView(self.view)
             
-            if self.greenBox.frame.contains(location) {
-                self.greenBox.center = location
+            if self.bear.frame.contains(location) {
+                self.bear.center = location
+            }
+            if self.monkey.frame.contains(location) {
+                self.monkey.center = location
+            }
+            if self.cuala.frame.contains(location) {
+                self.cuala.center = location
+            }
+            if self.crocodile.frame.contains(location) {
+                self.crocodile.center = location
+            }
+            if self.raccoon.frame.contains(location) {
+                self.raccoon.center = location
+            }
+            if self.chicken.frame.contains(location) {
+                self.chicken.center = location
+            }
+            
+            if CGRectIntersectsRect(bear.frame, bearShadow.frame) {
+                print("BEAR.")
+            }
+            if CGRectIntersectsRect(monkey.frame, monkeyShadow.frame) {
+                print("monkey.")
+            }
+            if CGRectIntersectsRect(cuala.frame, cualaShadow.frame) {
+                print("cuala.")
+            }
+            if CGRectIntersectsRect(crocodile.frame, crocodileShadow.frame) {
+                print("crocodile.")
+            }
+            if CGRectIntersectsRect(raccoon.frame, raccoonShadow.frame) {
+                print("raccoon.")
+            }
+            if CGRectIntersectsRect(chicken.frame, chickenShadow.frame) {
+                print("chicken.")
             }
         }
     }
