de7a3cdbca4f9bdfadc5ec33e4b0fcb69ccfce92
JennyShalai <jenny.shalai@gmail.com>
2016-09-11 17:02:50 -0400

    add collision detection, add movement detection, add z-index, fix overlapping and double dragging issues

--- educational-game-for-kids/AnimalsViewController.swift
+++ educational-game-for-kids/AnimalsViewController.swift
@@ -16,12 +16,23 @@ class AnimalsViewController: UIViewController {
     @IBOutlet weak var crocodile: UIImageView!
     @IBOutlet weak var cuala: UIImageView!
     @IBOutlet weak var raccoon: UIImageView!
+    
     @IBOutlet weak var chickenShadow: UIImageView!
     @IBOutlet weak var raccoonShadow: UIImageView!
     @IBOutlet weak var cualaShadow: UIImageView!
     @IBOutlet weak var crocodileShadow: UIImageView!
     
+    var isChickenMoving: Bool = false
+    var isCrocodileMoving: Bool = false
+    var isCualaMoving: Bool = false
+    var isRaccoonMoving: Bool = false
     
+    var isChickenMatch: Bool = false
+    var isCrocodileMatch: Bool = false
+    var isCualaMatch: Bool = false
+    var isRaccoonMatch: Bool = false
+    
+    let maxIndexZ: CGFloat = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,20 +46,35 @@ class AnimalsViewController: UIViewController {
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
+            
             let location = touch.locationInView(self.view)
            
-            if self.chicken.frame.contains(location) {
-                self.chicken.center = location
-            }
-            if self.crocodile.frame.contains(location) {
-                self.crocodile.center = location
-            }
-            if self.cuala.frame.contains(location) {
+            if self.cuala.frame.contains(location) && !isCualaMatch {
                 self.cuala.center = location
+                self.cuala.layer.zPosition = self.maxIndexZ
+                self.isCualaMoving = true
+                return
             }
             
-            if self.raccoon.frame.contains(location) {
+            if self.chicken.frame.contains(location) && !isChickenMatch {
+                self.chicken.center = location
+                self.chicken.layer.zPosition = self.maxIndexZ
+                self.isChickenMoving = true
+                return
+            }
+            
+            if self.crocodile.frame.contains(location) && !isCrocodileMatch {
+                self.crocodile.center = location
+                self.crocodile.layer.zPosition = self.maxIndexZ
+                self.isCrocodileMoving = true
+                return
+            }
+            
+            if self.raccoon.frame.contains(location) && !isRaccoonMatch {
                 self.raccoon.center = location
+                self.raccoon.layer.zPosition = self.maxIndexZ
+                self.isRaccoonMoving = true
+                return
             }
         }
     }
@@ -56,77 +82,100 @@ class AnimalsViewController: UIViewController {
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
+            
             let location = touch.locationInView(self.view)
             
-            if self.cuala.frame.contains(location) {
+            if self.cuala.frame.contains(location) && self.isCualaMoving {
                 self.cuala.center = location
             }
-            if self.crocodile.frame.contains(location) {
+            
+            if self.crocodile.frame.contains(location) && self.isCrocodileMoving {
                 self.crocodile.center = location
             }
-            if self.raccoon.frame.contains(location) {
+            
+            if self.raccoon.frame.contains(location) && self.isRaccoonMoving {
                 self.raccoon.center = location
             }
-            if self.chicken.frame.contains(location) {
+            
+            if self.chicken.frame.contains(location) && self.isChickenMoving {
                 self.chicken.center = location
-            }
-            
-            if CGRectIntersectsRect(chicken.frame, chickenShadow.frame) {
-                print("chicken.")
-                self.chicken.center = self.chickenShadow.center
-                self.chickenShadow.hidden = true
-                
-            }
-            
-            if CGRectIntersectsRect(cuala.frame, cualaShadow.frame) {
-                print("cuala.")
-                self.cuala.center = self.cualaShadow.center
-                self.cualaShadow.hidden = true
-                
-            }
-            
-            if CGRectIntersectsRect(crocodile.frame, crocodileShadow.frame) {
-                print("crocodile.")
-                self.crocodile.center = self.crocodileShadow.center
-                self.crocodileShadow.hidden = true
-            }
-            
-            if CGRectIntersectsRect(raccoon.frame, raccoonShadow.frame) {
-                print("raccoon.")
-                self.raccoon.center = self.raccoonShadow.center
-                self.raccoonShadow.hidden = true
-            
-            }
-            
-          
-            
-            if CGRectIntersectsRect(cuala.frame, crocodileShadow.frame) || CGRectIntersectsRect(cuala.frame, raccoonShadow.frame) || CGRectIntersectsRect(cuala.frame, chickenShadow.frame) {
-                print("WRONG CUALA.")
-                self.cuala.frame.origin.x = 20
-                self.cuala.frame.origin.y = 47
-            }
-            
-            if CGRectIntersectsRect(crocodile.frame, cualaShadow.frame) || CGRectIntersectsRect(crocodile.frame, raccoonShadow.frame) || CGRectIntersectsRect(crocodile.frame, chickenShadow.frame) {
-                print("WRONG CROCODILE.")
-                self.crocodile.frame.origin.x = 31
-                self.crocodile.frame.origin.y = 295
-            }
-            
-            if CGRectIntersectsRect(raccoon.frame, cualaShadow.frame) || CGRectIntersectsRect(raccoon.frame, crocodileShadow.frame) || CGRectIntersectsRect(raccoon.frame, chickenShadow.frame) {
-                print("WRONG RACCOON.")
-                self.raccoon.frame.origin.x = 32
-                self.raccoon.frame.origin.y = 433
-            }
-            
-            if CGRectIntersectsRect(chicken.frame, cualaShadow.frame) || CGRectIntersectsRect(chicken.frame, crocodileShadow.frame) || CGRectIntersectsRect(chicken.frame, raccoonShadow.frame) {
-                print("WRONG CHICKEN.")
-                self.chicken.frame.origin.x = 34
-                self.chicken.frame.origin.y = 169
             }
         }
     }
     
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.checkCollisions()
+        
+        self.isChickenMoving = false
+        self.isCrocodileMoving = false
+        self.isRaccoonMoving = false
+        self.isCualaMoving = false
+        
+        self.cuala.layer.zPosition = 4
+        self.chicken.layer.zPosition = 3
+        self.crocodile.layer.zPosition = 2
+        self.raccoon.layer.zPosition = 1
+    }
     
+    func checkCollisions() {
+        
+        if CGRectIntersectsRect(chicken.frame, chickenShadow.frame) {
+            
+            self.chicken.center = self.chickenShadow.center
+            self.chickenShadow.hidden = true
+            self.isChickenMatch = true
+            
+        }
+        
+        if CGRectIntersectsRect(cuala.frame, cualaShadow.frame) {
+            
+            self.cuala.center = self.cualaShadow.center
+            self.cualaShadow.hidden = true
+            self.isCualaMatch = true
+            
+        }
+        
+        if CGRectIntersectsRect(crocodile.frame, crocodileShadow.frame) {
+            
+            self.crocodile.center = self.crocodileShadow.center
+            self.crocodileShadow.hidden = true
+            self.isCrocodileMatch = true
+        }
+        
+        if CGRectIntersectsRect(raccoon.frame, raccoonShadow.frame) {
+            
+            self.raccoon.center = self.raccoonShadow.center
+            self.raccoonShadow.hidden = true
+            self.isRaccoonMatch = true
+        }
+        
+        
+        
+        if CGRectIntersectsRect(cuala.frame, crocodileShadow.frame) || CGRectIntersectsRect(cuala.frame, raccoonShadow.frame) || CGRectIntersectsRect(cuala.frame, chickenShadow.frame) {
+            
+            self.cuala.frame.origin.x = 20
+            self.cuala.frame.origin.y = 47
+        }
+        
+        if CGRectIntersectsRect(crocodile.frame, cualaShadow.frame) || CGRectIntersectsRect(crocodile.frame, raccoonShadow.frame) || CGRectIntersectsRect(crocodile.frame, chickenShadow.frame) {
+            
+            self.crocodile.frame.origin.x = 31
+            self.crocodile.frame.origin.y = 295
+        }
+        
+        if CGRectIntersectsRect(raccoon.frame, cualaShadow.frame) || CGRectIntersectsRect(raccoon.frame, crocodileShadow.frame) || CGRectIntersectsRect(raccoon.frame, chickenShadow.frame) {
+            
+            self.raccoon.frame.origin.x = 32
+            self.raccoon.frame.origin.y = 433
+        }
+        
+        if CGRectIntersectsRect(chicken.frame, cualaShadow.frame) || CGRectIntersectsRect(chicken.frame, crocodileShadow.frame) || CGRectIntersectsRect(chicken.frame, raccoonShadow.frame) {
+            
+            self.chicken.frame.origin.x = 34
+            self.chicken.frame.origin.y = 169
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- educational-game-for-kids/FruitsViewController.swift
+++ educational-game-for-kids/FruitsViewController.swift
@@ -22,10 +22,17 @@ class FruitsViewController: UIViewController {
     @IBOutlet weak var fruitShadowImg: UIImageView!
     @IBOutlet weak var avocadoShadowImg: UIImageView!
     
-    var isAvocadoMetch: Bool = false
-    var isOrangeMetch: Bool = false
-    var isFruitMetch: Bool = false
-    var isBarryMetch: Bool = false
+    var isAvocadoMoving: Bool = false
+    var isOrangeMoving: Bool = false
+    var isFruitMoving: Bool = false
+    var isBerryMoving: Bool = false
+    
+    var isAvocadoMatch: Bool = false
+    var isOrangeMatch: Bool = false
+    var isFruitMatch: Bool = false
+    var isBerryMatch: Bool = false
+    
+    let maxIndexZ: CGFloat = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,20 +46,35 @@ class FruitsViewController: UIViewController {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
+            
             let location = touch.locationInView(self.view)
             
-            if self.avocadoImg.frame.contains(location) {
+            if self.avocadoImg.frame.contains(location) && !isAvocadoMatch {
                 self.avocadoImg.center = location
-            }
-            if self.orangeImg.frame.contains(location) {
-                self.orangeImg.center = location
-            }
-            if self.fruitImg.frame.contains(location) {
-                self.fruitImg.center = location
+                self.avocadoImg.layer.zPosition = self.maxIndexZ
+                self.isAvocadoMoving = true
+                return
             }
             
-            if self.berryImg.frame.contains(location) {
+            if self.orangeImg.frame.contains(location) && !isOrangeMatch {
+                self.orangeImg.center = location
+                self.orangeImg.layer.zPosition = self.maxIndexZ
+                self.isOrangeMoving = true
+                return
+            }
+            
+            if self.fruitImg.frame.contains(location) && !isFruitMatch {
+                self.fruitImg.center = location
+                self.fruitImg.layer.zPosition = self.maxIndexZ
+                self.isFruitMoving = true
+                return
+            }
+            
+            if self.berryImg.frame.contains(location) && !isBerryMatch {
                 self.berryImg.center = location
+                self.berryImg.layer.zPosition = self.maxIndexZ
+                self.isBerryMoving = true
+                return
             }
         }
     }
@@ -60,80 +82,96 @@ class FruitsViewController: UIViewController {
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
+            
             let location = touch.locationInView(self.view)
             
-            if self.avocadoImg.frame.contains(location) {
+            if self.avocadoImg.frame.contains(location) && self.isAvocadoMoving {
                 self.avocadoImg.center = location
             }
-            if self.orangeImg.frame.contains(location) {
+            
+            if self.orangeImg.frame.contains(location) && self.isOrangeMoving {
                 self.orangeImg.center = location
             }
-            if self.fruitImg.frame.contains(location) {
+            
+            if self.fruitImg.frame.contains(location) && self.isFruitMoving {
                 self.fruitImg.center = location
             }
-            if self.berryImg.frame.contains(location) {
+            
+            if self.berryImg.frame.contains(location) && self.isBerryMoving {
                 self.berryImg.center = location
-            }
-            
-            if CGRectIntersectsRect(avocadoImg.frame, avocadoShadowImg.frame) {
-                print("AVOCADO.")
-                self.avocadoImg.center = self.avocadoShadowImg.center
-                self.avocadoShadowImg.hidden = true
-                self.isAvocadoMetch = true
-                
-            }
-            
-            if CGRectIntersectsRect(orangeImg.frame, orangeShadowImg.frame) {
-                print("ORANGE.")
-                self.orangeImg.center = self.orangeShadowImg.center
-                self.orangeShadowImg.hidden = true
-                self.isOrangeMetch = true
-                
-            }
-            
-            if CGRectIntersectsRect(fruitImg.frame, fruitShadowImg.frame) {
-                print("FRUIT.")
-                self.fruitImg.center = self.fruitShadowImg.center
-                self.fruitShadowImg.hidden = true
-                self.isFruitMetch = true
-            }
-            
-            if CGRectIntersectsRect(berryImg.frame, berryShadowImg.frame) {
-                print("BERRY.")
-                self.berryImg.center = self.berryShadowImg.center
-                self.berryShadowImg.hidden = true
-                self.isBarryMetch = true
-            }
-            
-            
-            
-            if CGRectIntersectsRect(avocadoImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(avocadoImg.frame, fruitShadowImg.frame) || CGRectIntersectsRect(avocadoImg.frame, berryShadowImg.frame) {
-                print("WRONG AVOCADO.")
-                self.avocadoImg.frame.origin.x = 45
-                self.avocadoImg.frame.origin.y = 42
-            }
-            
-            if CGRectIntersectsRect(orangeImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(orangeImg.frame, fruitShadowImg.frame) || CGRectIntersectsRect(orangeImg.frame, berryShadowImg.frame) {
-                print("WRONG ORANGE.")
-                self.orangeImg.frame.origin.x = 45
-                self.orangeImg.frame.origin.y = 172
-            }
-            
-            if CGRectIntersectsRect(fruitImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(fruitImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(fruitImg.frame, berryShadowImg.frame) {
-                print("WRONG FRUIT.")
-                self.fruitImg.frame.origin.x = 45
-                self.fruitImg.frame.origin.y = 298
-            }
-            
-            if CGRectIntersectsRect(berryImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(berryImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(berryImg.frame, fruitShadowImg.frame) {
-                print("WRONG BERRY.")
-                self.berryImg.frame.origin.x = 50
-                self.berryImg.frame.origin.y = 421
             }
         }
     }
     
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.checkCollisions()
+        
+        self.isAvocadoMoving = false
+        self.isOrangeMoving = false
+        self.isFruitMoving = false
+        self.isBerryMoving = false
+        
+        self.avocadoImg.layer.zPosition = 4
+        self.orangeImg.layer.zPosition = 3
+        self.fruitImg.layer.zPosition = 2
+        self.berryImg.layer.zPosition = 1
+    }
     
+    func checkCollisions() {
+        
+        if CGRectIntersectsRect(avocadoImg.frame, avocadoShadowImg.frame) {
+            
+            self.avocadoImg.center = self.avocadoShadowImg.center
+            self.avocadoShadowImg.hidden = true
+            self.isAvocadoMatch = true
+        }
+        
+        if CGRectIntersectsRect(orangeImg.frame, orangeShadowImg.frame) {
+            
+            self.orangeImg.center = self.orangeShadowImg.center
+            self.orangeShadowImg.hidden = true
+            self.isOrangeMatch = true
+        }
+        
+        if CGRectIntersectsRect(fruitImg.frame, fruitShadowImg.frame) {
+            
+            self.fruitImg.center = self.fruitShadowImg.center
+            self.fruitShadowImg.hidden = true
+            self.isFruitMatch = true
+        }
+        
+        if CGRectIntersectsRect(berryImg.frame, berryShadowImg.frame) {
+            
+            self.berryImg.center = self.berryShadowImg.center
+            self.berryShadowImg.hidden = true
+            self.isBerryMatch = true
+        }
+        
+        if CGRectIntersectsRect(avocadoImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(avocadoImg.frame, fruitShadowImg.frame) || CGRectIntersectsRect(avocadoImg.frame, berryShadowImg.frame) {
+            
+            self.avocadoImg.frame.origin.x = 45
+            self.avocadoImg.frame.origin.y = 42
+        }
+        
+        if CGRectIntersectsRect(orangeImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(orangeImg.frame, fruitShadowImg.frame) || CGRectIntersectsRect(orangeImg.frame, berryShadowImg.frame) {
+            
+            self.orangeImg.frame.origin.x = 45
+            self.orangeImg.frame.origin.y = 172
+        }
+        
+        if CGRectIntersectsRect(fruitImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(fruitImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(fruitImg.frame, berryShadowImg.frame) {
+            
+            self.fruitImg.frame.origin.x = 45
+            self.fruitImg.frame.origin.y = 298
+        }
+        
+        if CGRectIntersectsRect(berryImg.frame, avocadoShadowImg.frame) || CGRectIntersectsRect(berryImg.frame, orangeShadowImg.frame) || CGRectIntersectsRect(berryImg.frame, fruitShadowImg.frame) {
+            
+            self.berryImg.frame.origin.x = 50
+            self.berryImg.frame.origin.y = 421
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
